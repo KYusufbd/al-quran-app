a695b28d48069406e51f921b222a8a16fd1ca762
Kazi Yusuf <yfaka001@gmail.com>
2025-10-04 20:21:39 +0600

    Changed previous localhos urls which war forgotten to be changed

--- app/get-selected-authors/route.js
+++ app/get-selected-authors/route.js
@@ -1,13 +1,9 @@
 import { cookies } from 'next/headers';
 
-export async function GET(req, res) {
+export async function GET(rquest) {
   const cookieStore = await cookies();
-
-  res.setHeader('Access-Control-Allow-Origin', 'https://al-quran-app-blond.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   const { value } = cookieStore.get('auths');
 
-  return res.json(JSON.parse(value));
+  return Response.json(JSON.parse(value));
 }
