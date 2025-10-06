bc145935a6be38a13758dd8ac89de30715a05dc0
Kazi Yusuf <yfaka001@gmail.com>
2025-10-06 12:57:21 +0600

    Added fonts

--- app/al-quran/page.js
+++ app/al-quran/page.js
@@ -8,7 +8,7 @@ const AlQuranPage = async () => {
 
   return (
     <div className="flex flex-col w-full bg-base-300">
-      <div className="text-accent font-bold flex flex-col items-center p-6 bg-accent-content/60 gap-2">
+      <div className="text-accent font-bold flex flex-col items-center p-6 bg-primary gap-2">
         <h1 className="text-3xl">Al Quran</h1>
         <div className="flex flex-row gap-4 flex-wrap">
           <h3 className="text-lg">Total Surah: 114</h3>
@@ -19,9 +19,9 @@ const AlQuranPage = async () => {
         {surahNamesAr.map((name, index) => {
           return (
             <Link key={index} href={`/al-quran/${index + 1}`}>
-              <div className="w-full flex flex-col text-center justify-start items-center p-3 rounded-lg shadow-xl h-52 gap-3 bg-primary-content/80 text-primary font-bold">
+              <div className="w-full flex flex-col text-center justify-start items-center p-3 rounded-lg shadow-xl h-52 gap-5 bg-primary-content/80 text-primary font-normal">
                 <h3 className="text-base mb-3">{index + 1}</h3>
-                <h3 className="text-2xl">{name}</h3>
+                <h3 className="text-2xl font-medium font-amiri">{name}</h3>
                 <h3 className="text-2xl">{surahNamesEn[index]}</h3>
               </div>
             </Link>
