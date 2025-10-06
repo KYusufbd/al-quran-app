c41443619588d5b0cbac85f5c830c19d0a34cef1
Kazi Yusuf <yfaka001@gmail.com>
2025-10-06 13:21:02 +0600

    Made changes in spacing of surah page

--- app/al-quran/[surah]/page.js
+++ app/al-quran/[surah]/page.js
@@ -1,31 +1,31 @@
-import getSurahInfo from '@/app/lib/getSurahInfo';
-import getTranslations from '@/app/lib/getTranslations';
-import getVerses from '@/app/lib/getVerses';
-import { cookies } from 'next/headers';
+import getSurahInfo from "@/app/lib/getSurahInfo";
+import getTranslations from "@/app/lib/getTranslations";
+import getVerses from "@/app/lib/getVerses";
+import { cookies } from "next/headers";
 
 export default async function Surah({ params }) {
   const { surah } = await params;
   const authors = {
-    bn_ah_bayan: 'আহসানুল বায়ান', // 1
-    bn_fazlur_rah: 'ফজলুর রহমান', // 2
-    bn_m_khan: 'মুহিউদ্দীন খান', // 3
-    bn_mujibur_rah: 'মুজিবুর রহমান', // 4
-    bn_taisir: 'তাইসীরুল কুরআন', // 5
-    bn_ab_zakariya: 'আবু বকর মুহাম্মাদ যাকারিয়া', // 6
-    en_clear_quran: 'The Clear Quran', // 7
-    en_a_haleem: 'Abdul Haleem', // 8
-    en_jalalain: 'Jalalayn', // 9
-    en_khan_hilali: 'Khan and Hilali', // 10
-    en_taqi_usmani: 'Taqi Usmani', // 11
-    en_pickthall: 'Muhammad Marmaduke Pickthall', // 12
-    en_sahih_int: 'Saheeh International', // 13
-    en_transliteration: 'Transliteration', // 14
-    en_yusuf_ali: 'Yusuf Ali', // 15
+    bn_ah_bayan: "আহসানুল বায়ান", // 1
+    bn_fazlur_rah: "ফজলুর রহমান", // 2
+    bn_m_khan: "মুহিউদ্দীন খান", // 3
+    bn_mujibur_rah: "মুজিবুর রহমান", // 4
+    bn_taisir: "তাইসীরুল কুরআন", // 5
+    bn_ab_zakariya: "আবু বকর মুহাম্মাদ যাকারিয়া", // 6
+    en_clear_quran: "The Clear Quran", // 7
+    en_a_haleem: "Abdul Haleem", // 8
+    en_jalalain: "Jalalayn", // 9
+    en_khan_hilali: "Khan and Hilali", // 10
+    en_taqi_usmani: "Taqi Usmani", // 11
+    en_pickthall: "Muhammad Marmaduke Pickthall", // 12
+    en_sahih_int: "Saheeh International", // 13
+    en_transliteration: "Transliteration", // 14
+    en_yusuf_ali: "Yusuf Ali", // 15
   };
   const keys = Object.keys(authors);
 
   const cookieStore = await cookies();
-  const auths = cookieStore.get('auths');
+  const auths = cookieStore.get("auths");
   const authNumbers = auths?.value ? await JSON.parse(auths.value) : [];
 
   const auth = [];
@@ -34,7 +34,8 @@ export default async function Surah({ params }) {
   });
 
   if (surah < 115 && surah > 0) {
-    const { name_en, name_ar, name_bn, surah_start, ayah_count } = await getSurahInfo(surah);
+    const { name_en, name_ar, name_bn, surah_start, ayah_count } =
+      await getSurahInfo(surah);
     const verses = await getVerses(surah_start, ayah_count);
     const translations = await getTranslations(surah_start, ayah_count, auth);
 
@@ -42,7 +43,9 @@ export default async function Surah({ params }) {
       <div className="font-sans flex flex-col items-center min-h-screen px-8 pb-8 gap-16 bg-base-100">
         <main className="flex flex-col gap-4 w-160 max-w-full items-center text-center">
           <div className="text-accent font-bold flex flex-col items-center p-6 bg-primary rounded-sm gap-4 w-full">
-            <h1 className="text-4xl font-amiri font-medium">سُوْرَةُ {name_ar}</h1>
+            <h1 className="text-4xl font-amiri font-medium">
+              سُوْرَةُ {name_ar}
+            </h1>
             <div className="flex flex-row gap-4 flex-wrap">
               <p>{`${surah}. Surah ${name_en}.`}</p>
               <p>Total ayah: {ayah_count}</p>
@@ -60,18 +63,26 @@ export default async function Surah({ params }) {
             </div>
           </div> */}
 
-          <div className="flex flex-col gap-4">
-            {verses.map((verse) => {
-              const trans = translations[verses.indexOf(verse)];
+          <div className="flex flex-col gap-2">
+            {verses.map((verse, index) => {
+              const trans = translations[index];
               const ayah_no = verse.no - surah_start + 1;
               return (
-                <div id={ayah_no} className="flex flex-col gap-3 shadow-2xl rounded-3xl p-6 bg-primary-content/75" key={verses.indexOf(verse)}>
-                  <p className="text-accent text-center font-bold mx-auto -mt-3">{ayah_no}</p>
-                  <p className="text-3xl text-primary font-hafs leading-14 p-4 w-full rounded-xl bg-primary/10">{verse.indo_pak}</p>
-                  <div className="w-full text-start flex flex-col gap-6">
-                    {auth.map((au) => {
+                <div
+                  id={ayah_no}
+                  className="flex flex-col gap-3 shadow-2xl rounded-xl p-3 pt-4 bg-primary-content/75"
+                  key={index}
+                >
+                  <p className="text-accent text-center font-bold -mb-3 mx-auto -mt-4">
+                    {ayah_no}
+                  </p>
+                  <p className="text-3xl text-primary font-hafs leading-14 p-1 w-full rounded-xl bg-primary/10">
+                    {verse.indo_pak}
+                  </p>
+                  <div className="w-full text-start flex flex-col gap-4">
+                    {auth.map((au, index) => {
                       return (
-                        <div className="w-full flex flex-col gap-2" key={auth.indexOf(au)}>
+                        <div className="w-full flex flex-col gap-1" key={index}>
                           <h5 className="text-secondary">{authors[au]}</h5>
                           <p className="text-primary text-xl">{trans[au]}</p>
                         </div>
