198654209ca77332be91d8e30ab0b9f780d00ab0
Kazi Yusuf <yfaka001@gmail.com>
2025-10-08 10:22:36 +0600

    Added loading page

---
+++ app/al-quran/[surah]/loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className="flex w-full h-screen bg-base-100 justify-center items-center">
+      <span className="loading loading-bars text-primary loading-xl"></span>
+    </div>
+  );
+};
+
+export default Loading;
